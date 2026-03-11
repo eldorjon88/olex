6651ec71021ae2f6d52353c778cc69214fd8cd4d
eldorjon88 <eldorjonturamurodov2@gmail.com>
2026-03-11 08:44:52 +0500

    market

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import CustomUser
+from .models import CustomUser, SellerProfile
 
-# Register your models here.
 admin.site.register(CustomUser)
+admin.site.register(SellerProfile)
--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category, Product, ProductImage, Favorite, Order, Review
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(Favorite)
+admin.site.register(Order)
+admin.site.register(Review)
